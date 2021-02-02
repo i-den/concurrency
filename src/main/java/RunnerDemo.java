a21d95d6b551528ced41771ad8f8bbd58d4116b9
iVan <i-den@github.com>
2021-02-02 22:54:21 +0200

    Added Worker/Daemon demo

--- src/main/java/RunnerDemo.java
+++ src/main/java/RunnerDemo.java
@@ -1,19 +1,25 @@
 public class RunnerDemo {
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(new RunnerA());
+        Thread t1 = new RunnerA();
         Thread t2 = new Thread(new RunnerB());
 
         t1.start();
         t2.start();
+        System.out.println("Main Method");
     }
 }
 
-class RunnerA implements Runnable {
+class RunnerA extends Thread {
 
     @Override
     public void run() {
         for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("RunnerA : " + i);
         }
     }
@@ -24,6 +30,11 @@ class RunnerB implements Runnable {
     @Override
     public void run() {
         for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("RunnerB : " + i);
         }
     }
---
+++ src/main/java/WorkerDemo.java
@@ -0,0 +1,42 @@
+import com.sun.corba.se.spi.orbutil.threadpool.Work;
+
+public class WorkerDemo {
+
+    public static void main(String[] args) {
+        Thread d1 = new Thread(new Daemon());
+        Thread w1 = new Thread(new Worker());
+
+        d1.setDaemon(true);
+
+        d1.start();
+        w1.start(); // d1 will die once w1 dies
+    }
+}
+
+class Worker implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Worker#run on thread:" + Thread.currentThread().getName());
+    }
+}
+
+class Daemon implements Runnable {
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Daemon#run on thread: " + Thread.currentThread().getName());
+        }
+    }
+}
