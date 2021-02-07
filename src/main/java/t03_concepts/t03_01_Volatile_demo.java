60d79f7421bc50e672db68b892e459b17063badc
iVan <i-den@github.com>
2021-02-07 19:56:41 +0200

    Added Content

---
+++ src/main/java/t03_concepts/t03_01_Volatile_demo.java
@@ -0,0 +1,48 @@
+package t03_concepts;
+
+public class t03_01_Volatile_demo {
+
+    public static void main(String[] args) {
+        Worker w = new Worker();
+        Thread t1 = new Thread(w);
+        Thread t2 = new Thread(w);
+
+        w.setRunning(true); // issue
+        t1.start();
+        t2.start();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        w.setRunning(false); // issue
+        System.out.println("End");
+    }
+}
+
+class Worker implements Runnable {
+
+    private boolean running; // could be volatile
+
+    @Override
+    public void run() {
+        while (running) {
+            System.out.println("Working on thread " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+}
